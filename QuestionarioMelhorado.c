cc6e16168fe4acd63ce95d866a8c1baf28f14383
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-09-12 19:38:55 -0300

    Update QuestionarioMelhorado.c

--- QuestionarioMelhorado.c
+++ QuestionarioMelhorado.c
@@ -12,6 +12,7 @@ typedef struct
 void apresentarPergunta(Pergunta pergunta, FILE *arquivo)
 {
     char opcao;
+    printf("\n");
     printf("%s\n\n", pergunta.pergunta);
 
     for (int i = 0; i < 4; i++)
@@ -67,11 +68,11 @@ int main()
 
         Pergunta perguntasSantosDumont[] =
         {
-            {"Qual foi a primeira invenção de Santos Dumont?", {"Balão cilíndrico inflado a hidrogênio.", "Modelo de aeroplano", "Motor movido por combustível", "A primeira asa de avião do mundo"}, 'A'},
-            {"Em qual estado do Brasil nasceu Santos Dumont?", {"Distrito Federal.", "Mato Grosso", "São Paulo", "Minas Gerais."}, 'D'},
-            {"Quais eram as profissões de Dumont?", {"Inventor", "Cientista", "Aeronauta e inventor.", "Aeronalta"}, 'C'},
-            {"Quando foi realizado o primeiro voo do avião 14-Bis?", {"23 de outubro de 1906 em Paris.", "5 de maio de 1907 em Belo Horizonte.", "15 de dezembro de 1903 em Kitty Hawk.", "17 de dezembro de 1903 em Paris."}, 'A'},
-            {"Com quantos anos morreu Santos Dumont?", {"50 anos.", "59 anos.", "68 anos.", "72 anos."}, 'B'}
+            {"1. Qual foi a primeira invenção de Santos Dumont?", {"Balão cilíndrico inflado a hidrogênio.", "Modelo de aeroplano", "Motor movido por combustível", "A primeira asa de avião do mundo"}, 'A'},
+            {"2. Em qual estado do Brasil nasceu Santos Dumont?", {"Distrito Federal.", "Mato Grosso", "São Paulo", "Minas Gerais."}, 'D'},
+            {"3. Quais eram as profissões de Dumont?", {"Inventor", "Cientista", "Aeronauta e inventor.", "Aeronalta"}, 'C'},
+            {"4. Quando foi realizado o primeiro voo do avião 14-Bis?", {"23 de outubro de 1906 em Paris.", "5 de maio de 1907 em Belo Horizonte.", "15 de dezembro de 1903 em Kitty Hawk.", "17 de dezembro de 1903 em Paris."}, 'A'},
+            {"5. Com quantos anos morreu Santos Dumont?", {"50 anos.", "59 anos.", "68 anos.", "72 anos."}, 'B'}
         };
 
         for (int i = 0; i < 5; i++)
@@ -85,11 +86,11 @@ int main()
 
         Pergunta perguntasOlimpiadas[] =
         {
-            {"Quais são as duas novas modalidades que entraram nessas edições das Olimpíadas?", {"Skateboarding e escalada esportiva.", "BMX e natação artística.", "Breaking e canoagem slalom extremo.", "Tênis de mesa e esgrima."}, 'C'},
-            {"Quais os anos em que Paris já foi sede dos Jogos Olímpicos?", {"Nunca foi sede", "Apenas em 2024", "1800 e 1920 e agora em 2024", "1900 e 1924 e agora em 2024"}, 'D'},
-            {"Quantas modalidades de esporte serão ao todo na edição inteira?", {"O evento terá 58 modalidades e 32 esportes.", "O evento terá 8 modalidades e 2 esportes.", "O evento terá 48 modalidades e 32 esportes.", "O evento terá 48 modalidades e 22 esportes."}, 'C'},
-            {"Qual o nome do mascote desta edição?", {"Phryges", "Bolinha", "Willie", "Footix"}, 'A'},
-            {"Em qual dia será a abertura dos jogos?", {"27 de julho.", "26 de julho.", "30 de janeiro.", "31 de abril."}, 'B'}
+            {"1. Quais são as duas novas modalidades que entraram nessas edições das Olimpíadas?", {"Skateboarding e escalada esportiva.", "BMX e natação artística.", "Breaking e canoagem slalom extremo.", "Tênis de mesa e esgrima."}, 'C'},
+            {"2. Quais os anos em que Paris já foi sede dos Jogos Olímpicos?", {"Nunca foi sede", "Apenas em 2024", "1800 e 1920 e agora em 2024", "1900 e 1924 e agora em 2024"}, 'D'},
+            {"3. Quantas modalidades de esporte serão ao todo na edição inteira?", {"O evento terá 58 modalidades e 32 esportes.", "O evento terá 8 modalidades e 2 esportes.", "O evento terá 48 modalidades e 32 esportes.", "O evento terá 48 modalidades e 22 esportes."}, 'C'},
+            {"4. Qual o nome do mascote desta edição?", {"Phryges", "Bolinha", "Willie", "Footix"}, 'A'},
+            {"5. Em qual dia será a abertura dos jogos?", {"27 de julho.", "26 de julho.", "30 de janeiro.", "31 de abril."}, 'B'}
         };
 
         for (int i = 0; i < 5; i++)
